8becc91102673c94ad8bdd690a5403f5c79422b6
olja88 <oljalatinovic88@gmail.com>
2014-02-28 11:09:50 +0100

    Obradjen exception kada nema tabele u populatoru. Za punjenje podataka kada se drop-uju tabele

--- src/main/java/com/oljalatinovic/oljaee/service/DBPopulator.java
+++ src/main/java/com/oljalatinovic/oljaee/service/DBPopulator.java
@@ -48,33 +48,58 @@ public class DBPopulator {
 
     @PostConstruct
     private void populateDB() {        
-        serbia = countryService.findCountry("SERBIA");
+        try {
+            serbia = countryService.findCountry("SERBIA");
+        } catch (Exception e) {
+            serbia = new Country("SR", "SERBIA", "Serbia", "SER", "111");
+            countryService.createCountry(serbia);
+        }
         if (serbia == null) {
             serbia = new Country("SR", "SERBIA", "Serbia", "SER", "111");
             countryService.createCountry(serbia);
         }
 
-        user = userService.findUser("user");
+        try {
+            user = userService.findUser("user");
+        } catch (Exception e) {
+            user = new Users("User", "User", "user", "user", "user@oljaee.com", new Address("Moja ulica i broj", "A i grad", "44", new Country("SR", "SERBIA", "Serbia", "SER", "1")));
+            userService.createUser(user);
+        }            
         if (user == null) {
             user = new Users("User", "User", "user", "user", "user@oljaee.com", new Address("Moja ulica i broj", "A i grad", "44", new Country("SR", "SERBIA", "Serbia", "SER", "1")));
             userService.createUser(user);
         }
 
-        admin = userService.findUser("admin");
+        try {
+            admin = userService.findUser("admin");
+        } catch (Exception e) {
+            admin = new Users("Admin", "Admin", "admin", "admin", "admin@oljaee.com", new Address("Takodje samo 44", "Grad do", "32", new Country("SR", "SERBIA", "Serbia", "SER", "1")));
+            userService.createUser(admin);
+        }              
         if (admin == null) {
             admin = new Users("Admin", "Admin", "admin", "admin", "admin@oljaee.com", new Address("Takodje samo 44", "Grad do", "32", new Country("SR", "SERBIA", "Serbia", "SER", "1")));
             userService.createUser(admin);
         }
 
-        olja = userService.findUser("olja88");
+        try {
+            olja = userService.findUser("olja88");
+        } catch (Exception e) {
+            olja = new Users("Olja", "Latinović", "olja88", "password1", "olja@oljaee.com", new Address("Licau jbro55", "Dgra", "12", new Country("SR", "SERBIA", "Serbia", "SER", "1")));
+            userService.createUser(olja);
+        }            
         if (olja == null) {
             olja = new Users("Olja", "Latinović", "olja88", "password1", "olja@oljaee.com", new Address("Licau jbro55", "Dgra", "12", new Country("SR", "SERBIA", "Serbia", "SER", "1")));
             userService.createUser(olja);
         }
         
-        administracija = mainMenuService.findMainMenu("Administracija");
+        try {
+            administracija = mainMenuService.findMainMenu("Administracija");
+        } catch (Exception e) {
+            administracija = new MainMenu("Administracija", "Administracija", "Administracija", olja, olja, new Date(), new Date(), null /*Boolean.TRUE*/ , null, "imgAdmin");
+            mainMenuService.createMainMenu(administracija);
+        }            
         if (administracija == null) {
-            administracija = new MainMenu("Administracija", "Administracija", "Administracija", olja, olja, new Date(), new Date(), Boolean.TRUE , null, "imgAdmin");
+            administracija = new MainMenu("Administracija", "Administracija", "Administracija", olja, olja, new Date(), new Date(), null /*Boolean.TRUE*/ , null, "imgAdmin");
             mainMenuService.createMainMenu(administracija);
         }        
     }
