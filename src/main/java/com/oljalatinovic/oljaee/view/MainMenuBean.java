6d9656619e609e01f193105b9bee6196f220e36c
Olja Latinović <oljalatinovic88@gmail.com>
2014-03-01 00:24:54 +0100

    Doradjen meni. Punjenje liste menija programski dok se ne sredi servis za selekt iz baze

--- src/main/java/com/oljalatinovic/oljaee/view/MainMenuBean.java
+++ src/main/java/com/oljalatinovic/oljaee/view/MainMenuBean.java
@@ -4,6 +4,8 @@ import com.oljalatinovic.oljaee.entity.MainMenu;
 import com.oljalatinovic.oljaee.service.MainMenuService;
 import com.oljalatinovic.oljaee.util.Loggable;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
@@ -17,15 +19,21 @@ import javax.inject.Named;
 @SessionScoped
 @Loggable
 @CatchException
-public class MainMenuBean  extends AbstractBean implements Serializable {
+public class MainMenuBean extends AbstractBean implements Serializable {
     
-    private final List<MainMenu> meni;
+//    private final List<MainMenu> meni = new ArrayList<>();
+    private List<MainMenu> meni;
     
-    @Inject
-    private MainMenuService mainService;
+//    @Inject
+//    private MainMenuService mainService;
 
     public MainMenuBean() {        
-        meni  = mainService.findAllMainMenus();
+//        meni.addAll(mainService.findAllMainMenus());
+//        meni.add(mainService.findMainMenu("Administracija"));
+        meni = new ArrayList<MainMenu>();  
+          
+        meni.add(new MainMenu("Administracija", "Administracija", "Administracija", null, null, new Date(), new Date(), Boolean.TRUE , null, "imgAdmin"));
+        meni.add(new MainMenu("Sifarnici", "Sifarnici", "Sifarnici", null, null, new Date(), new Date(), Boolean.TRUE , null, "imgAdmin"));
     }
 
     public List<MainMenu> getMeni() {
